625910ab8013d1f537fee9bfb60f33b07527bb78
Kron98 <jorge.gutierrez36@yahoo.es>
2021-09-12 12:37:30 +0200

    Update nftealand.js

--- nftealand.js
+++ nftealand.js
@@ -1,11 +1,11 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <audio muted autoplay loop id="sound1" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1624298129/NFTea_Main_Day_song_pjpejd.mp3"></audio>
-<audio id="arcade1" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190630/Arcade_1_Explosion_d7la3l.wav"></audio>
-<audio id="arcade2" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/Arcade_2_Blip_w__Ping_xpjgge.wav"></audio>
-<audio id="arcade3" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/Arcade_3_Jump_vehzg8.wav"></audio>
-<audio id="arcade4" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190634/Arcade_4_Falling_qpajyk.wav"></audio>
-<audio id="arcade5" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/Arcade_5_Crunch_sorgyc.wav"></audio>
-<audio id="arcade6" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/Arcade_6_jump_2_uafrfx.wav"></audio>
+<audio id="arcade1" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190630/Arc1.wav"></audio>
+<audio id="arcade2" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/Arc2.wav"></audio>
+<audio id="arcade3" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/Arc3.wav"></audio>
+<audio id="arcade4" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190634/Arc4.wav"></audio>
+<audio id="arcade5" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/Arc5.wav"></audio>
+<audio id="arcade6" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/Arc6.wav"></audio>
 <script>
 var song1 = $('#arcade1');
 var song2 = $('#arcade2');
@@ -13,143 +13,125 @@ var song3 = $('#arcade3');
 var song4 = $('#arcade4');
 var song5 = $('#arcade5');
 var song6 = $('#arcade6');
-var audioArray1 = [song1, song2, song3, song4, song5, song6];
+var Ar1 = [song1, song2, song3, song4, song5, song6];
 var i = -1;
 $("a#arcade").click(function() {
   if (i >= 0) {
-    audioArray1[i].currentTime = 0;
-    audioArray1[i].trigger('pause');
+    Ar1[i].currentTime = 0;
+    Ar1[i].trigger('pause');
   }
   i++;
-  if (i >= audioArray1.length)
+  if (i >= Ar1.length)
     i = 0;
-  audioArray1[i].trigger('play');
+  Ar1[i].trigger('play');
 });
 </script>
-<audio id="bubbles" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/Bubbles_Home_Page_kjb3jf.wav"></audio>
-<audio id="bubbles2" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/Bubbles_Home_Page_kjb3jf.wav"></audio>
+<audio id="bubbles" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/bubb1.wav"></audio>
+<audio id="bubbles2" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/bubb1.wav"></audio>
 <script>
 var song1 = $('#bubbles');
-var audioArray5 = [song1];
+var Array5 = [song1, song2];
 var i = -1;
 $("a#bubble").click(function() {
   if (i >= 0) {
-    audioArray5[i].currentTime = 0;
-    audioArray5[i].trigger('pause');
+    Array5[i].currentTime = 0;
+    Array5[i].trigger('pause');
   }
   i++;
-  if (i >= audioArray5.length)
+  if (i >= Array5.length)
     i = 0;
-  audioArray5[i].trigger('play');
+  Array5[i].trigger('play');
 });
 </script>
-<audio id="drinkbar1" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190632/Drink_Bar_Sound_1_suhjm4.wav"></audio>
-<audio id="drinkbar2" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190632/Drink_Bar_Sound_2_orq3iv.wav"></audio>
+<audio id="drinkbar1" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190632/bar1.wav"></audio>
+<audio id="drinkbar2" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190632/bar2.wav"></audio>
 <script>
 var song1 = $('#drinkbar1');
-var song2 = $('#drinkbar');
-var audioArray2 = [song1, song2];
+var song2 = $('#drinkbar2');
+var Array2 = [song1, song2];
 var i = -1;
 $("a#drinkbar").click(function() {
   if (i >= 0) {
-    audioArray2[i].currentTime = 0;
-    audioArray2[i].trigger('pause');
+    Array2[i].currentTime = 0;
+    Array2[i].trigger('pause');
   }
   i++;
-  if (i >= audioArray2.length)
+  if (i >= Array2.length)
     i = 0;
-  audioArray2[i].trigger('play');
+  Array2[i].trigger('play');
 });
 </script>
-<audio id="sign1" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190632/NFTea_Land_Sign_nymfs6.wav"></audio>
+<audio id="sign1" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190632/sgn.wav"></audio>
 <script type="text/javascript">
 var song1 = $('#sign1');
-var audioArray6 = [song1];
+var Array6 = [song1];
 var i = -1;
 $("a#sign").click(function() {
   if (i >= 0) {
-    audioArray6[i].currentTime = 0;
-    audioArray6[i].trigger('pause');
+    Array6[i].currentTime = 0;
+    Array6[i].trigger('pause');
   }
   i++;
-  if (i >= audioArray6.length)
+  if (i >= Array6.length)
     i = 0;
 
-  audioArray6[i].trigger('play');
+  Array6[i].trigger('play');
 });
 </script>
-<audio id="pinball1" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/Pinball_1_lcbsmo.wav"></audio>
-<audio id="pinball2" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190630/PinballBall_2_oniihf.wav"></audio>
+<audio id="pinball1" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190631/Pb1.wav"></audio>
+<audio id="pinball2" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190630/Pb2.wav"></audio>
 <script>
 var song1 = $('#pinball1');
 var song2 = $('#pinball2');
-var audioArray3 = [song1, song2];
+var Array3 = [song1, song2];
 var i = -1;
 $("a#pinball").click(function() {
   if (i >= 0) {
-    audioArray3[i].currentTime = 0;
-    audioArray3[i].trigger('pause');
+    Array3[i].currentTime = 0;
+    Array3[i].trigger('pause');
   }
   i++;
-  if (i >= audioArray3.length)
+  if (i >= Array3.length)
     i = 0;
-  audioArray3[i].trigger('play');
+  Array3[i].trigger('play');
 });
 </script>
-<audio id="prize1" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190630/Prize_Room_1_ytsj16.wav"></audio>
-<audio id="prize2" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190630/Prize_Room_2_ubd4m2.wav"></audio>
+<audio id="prize1" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190630/Prz1.wav"></audio>
+<audio id="prize2" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190630/Prz2.wav"></audio>
 <script>
 var song1 = $('#prize1');
 var song2 = $('#prize2');
-var audioArray4 = [song1, song2];
+var Ar4 = [song1, song2];
 var i = -1;
 $("a#prize").click(function() {
 
   if (i >= 0) {
-    audioArray4[i].currentTime = 0;
-    audioArray4[i].trigger('pause');
+    Ar4[i].currentTime = 0;
+    Ar4[i].trigger('pause');
   }
   i++;
-  if (i >= audioArray4.length)
+  if (i >= Ar4.length)
     i = 0;
 
-  audioArray4[i].trigger('play');
+  Ar4[i].trigger('play');
 });
 </script>
-<audio id="recycle" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190630/Recycle_Machine_fge5zs.wav"></audio>
+<audio id="recycle" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629190630/Recy.wav"></audio>
 <script type="text/javascript">
 var song1 = $('#recycle');
-var audioArray7 = [song1];
+var Ar7 = [song1];
 var i = -1;
 $("a#recycle1").click(function() {
 
   if (i >= 0) {
-    audioArray7[i].currentTime = 0;
-    audioArray7[i].trigger('pause');
+    Ar7[i].currentTime = 0;
+    Ar7[i].trigger('pause');
   }
   i++;
-  if (i >= audioArray7.length)
+  if (i >= Ar7.length)
     i = 0;
 
-  audioArray7[i].trigger('play');
-});
-</script>
-<audio id="doorbottom" src="https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/Metal_Door_Rattle_2_tqsrl3.wav"></audio>
-<script type="text/javascript">
-var song1 = $('#metaldoorbott');
-var audioArray8 = [song1];
-var i = -1;
-$("a#doorbottom").click(function() {
-
-  if (i >= 0) {
-    audioArray8[i].currentTime = 0;
-    audioArray8[i].trigger('pause');
-  }
-  i++;
-  if (i >= audioArray8.length)
-    i = 0;
-
-  audioArray8[i].trigger('play');
+  Ar7[i].trigger('play');
 });
 </script>
 <script>
@@ -181,16 +163,13 @@ $("a#doorbottom").click(function() {
   audio.volume = 0.8;
 </script>
 
-<script type="text/javascript">
+<script>
 $("#muteaudio").click(function() {
         var bool = $("audio").prop("muted");
         $("audio").prop("muted",!bool);
 });
 </script>
-
-
-
-<script type="text/javascript">
+<script>
 function toggleSound(img)
 {
   img.src= img.src=="https://res.cloudinary.com/dh2ufbkjp/image/upload/v1630080736/NFTea_Volume_ON_DAY_Alpha-export_aenomf.png" ? "https://res.cloudinary.com/dh2ufbkjp/image/upload/v1630080733/NFTea_Volume_OFF_DAY_Alpha-export_ma3wxo.png" : "https://res.cloudinary.com/dh2ufbkjp/image/upload/v1630080736/NFTea_Volume_ON_DAY_Alpha-export_aenomf.png";
@@ -214,65 +193,56 @@ function toggleSound(img)
 var date = new Date();
 var hour = date.getHours();
 if(hour >= 7 && hour < 18) {
-  document.getElementById("sound1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1624298129/NFTea_Main_Day_song_pjpejd.mp3";
+  document.getElementById("sound1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1624298129/NFTea_M_D.mp3";
 }else{
-  document.getElementById("sound1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629018126/NFTea_Main_Nighttime_song_huioam.mp3";
+  document.getElementById("sound1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1629018126/NFTea_M_N.mp3";
 }
 </script>
 
 <script>
-var date = new Date();
-var hour = date.getHours();
+var d = new Date();
+var h = date.getHours();
+console.log(h)
+var m = date.getMinutes();
+const e = document.querySelector(".viewport-div");
 
-var minutes = date.getMinutes();
-const el = document.querySelector(".viewport-div");
-const al = document.querySelector(".drinkbar");
-const door = document.querySelector(".metaldoorbott");
-
-if(hour >=7 && hour < 18) {
-console.log(hour)
-    	document.getElementById("gif").src = "https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/61294ee350d0b77f38663805_NFTEA_homepage_Day_Closed_01.gif";
-      	document.getElementById("drinkbar").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/Metal_Door_Rattle_3_x775rz.wav";
-	document.getElementById("prize1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/Metal_Door_Rattle_1_wlxmmh.wav";
-     	document.getElementById("prize2").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/Metal_Door_Rattle_1_wlxmmh.wav";
-     	el.style.backgroundImage =
+if(h >=7 && h < 18) {
+    document.getElementById("gif").src = "https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/61294ee350d0b77f38663805_NFTEA_homepage_Day_Closed_01.gif";
+     document.getElementById("prize1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD1.wav";
+     document.getElementById("drinkbar1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD3.wav";
+     document.getElementById("drinkbar2").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD3.wav";
+     document.getElementById("prize2").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD1.wav";
+     e.style.backgroundImage =
        "url(https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/60d0476f5b485d3d0ac65254_Tall_Boy_-_NFTEA_LAND_-_cloud_animation-small-export200.gif)";
-	door.style.display = "block";
-       
-} else if (hour >= 18 && hour < 21) {
-console.log(hour)
-
-    	document.getElementById("gif").src = "https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/613a1412865fc84d51229463_NFTEA_LAND_CLOSED_Night.gif";
-	document.getElementById("drinkbar").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/Metal_Door_Rattle_3_x775rz.wav";         
-	document.getElementById("prize2").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/Metal_Door_Rattle_1_wlxmmh.wav";
-    	document.getElementById("prize1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/Metal_Door_Rattle_1_wlxmmh.wav";
-    	el.style.backgroundImage =
+} else if (h >= 18 && h < 21) {
+    document.getElementById("gif").src = "https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/613a1412865fc84d51229463_NFTEA_LAND_CLOSED_Night.gif";
+    document.getElementById("prize1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD1.wav";
+    document.getElementById("prize2").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD1.wav";
+    document.getElementById("drinkbar1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD3.wav";
+    document.getElementById("drinkbar2").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD3.wav";
+    e.style.backgroundImage =
       "url(https://res.cloudinary.com/dh2ufbkjp/image/upload/v1628271809/Night_Clouds_seq_02_jpofwh.gif)";
-	door.style.display = "block";
-} else if ((hour >= 21 && hour <= 21) && (minutes >= 0 && minutes <= 30)) {
-console.log(hour)
+} else if ((h == 21) && (m >= 0 && m <= 30)) {
    document.getElementById("gif").src = "https://res.cloudinary.com/dh2ufbkjp/image/upload/v1631197608/NFTEA_LAND_CLOSED_BarOpen_9to930pm_poywvw.gif";
-   el.style.backgroundImage = "url(https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/612f66b141a07b960d68fffa_Starry_Night_V2.2.gif)";
-   al.href = "https://discord.gg/eMtc3ABB";
-   door.style.display = "none";
-} else if ((minutes >= 30 && minutes <= 59) && (hour >= 21 && hour < 5)) {
-console.log(hour)
-   document.getElementById("gif").src = "https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/613a1412865fc84d51229463_NFTEA_LAND_CLOSED_Night.gif";
-	document.getElementById("prize1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/Metal_Door_Rattle_1_wlxmmh.wav";
-	document.getElementById("drinkbar").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/Metal_Door_Rattle_3_x775rz.wav";     
-	document.getElementById("prize2").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/Metal_Door_Rattle_1_wlxmmh.wav";
-	el.style.backgroundImage =
-  	"url(https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/612f66b141a07b960d68fffa_Starry_Night_V2.2.gif)";
-	door.style.display = "block";
-} else if (hour >= 5 && hour < 7) {
-console.log(hour)
-   document.getElementById("gif").src = "https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/613a1412865fc84d51229463_NFTEA_LAND_CLOSED_Night.gif";
-	document.getElementById("drinkbar").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/Metal_Door_Rattle_3_x775rz.wav";
-	document.getElementById("prize1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/Metal_Door_Rattle_1_wlxmmh.wav";
-     document.getElementById("prize2").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/Metal_Door_Rattle_1_wlxmmh.wav";
-	door.style.display = "block";
-el.style.backgroundImage =
+  e.style.backgroundImage =
+    "url(https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/612f66b141a07b960d68fffa_Starry_Night_V2.2.gif)";
+    a.href =
+    "https://discord.gg/eMtc3ABB";
+} else if ( ((h == 21) && (m >= 30 && m <= 59)) || (h >= 22 && h <= 23) || (h >= 0 && h < 5)) {
+document.getElementById("gif").src = "https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/613a1412865fc84d51229463_NFTEA_LAND_CLOSED_Night.gif";
+document.getElementById("prize1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD1.wav";
+document.getElementById("prize2").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD1.wav";
+document.getElementById("drinkbar1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD3.wav";
+document.getElementById("drinkbar2").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD3.wav";
+e.style.backgroundImage =
+  "url(https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/612f66b141a07b960d68fffa_Starry_Night_V2.2.gif)";
+} else if (h >= 5 && h < 7) {
+  document.getElementById("gif").src = "https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/613a1412865fc84d51229463_NFTEA_LAND_CLOSED_Night.gif";
+	document.getElementById("prize1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD1.wav";
+  document.getElementById("prize2").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD1.wav";
+  document.getElementById("drinkbar1").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD3.wav";
+  document.getElementById("drinkbar2").src = "https://res.cloudinary.com/dh2ufbkjp/video/upload/v1631379553/MD3.wav";
+e.style.backgroundImage =
   "url(https://uploads-ssl.webflow.com/60ae8c4857598e31ccef08c2/613a1412865fc84d51229463_NFTEA_LAND_CLOSED_Night.gif)";
-
 }
 </script>
